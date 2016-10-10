f280fd0bd49e064a7842e6b54abd71e3c893db99
João Marcos Stefani <joaomstefani@gmail.com>
2016-10-10 12:06:08 -0300

    Banner

--- database/migrations/2016_09_08_173613_create_banner_table.php
+++ database/migrations/2016_09_08_173613_create_banner_table.php
@@ -12,7 +12,20 @@ class CreateBannerTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('banners', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id')->unsigned();
+            $table->unsignedInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
+            $table->string('localizacao', 100);
+            $table->date('datainicio');
+            $table->date('datafinal');
+            $table->string('foto_banner', 100);
+            $table->string('link', 200);
+            $table->tinyInteger('ativo',0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
     }
 
     /**
@@ -22,6 +35,6 @@ class CreateBannerTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::drop('banners');
     }
 }
