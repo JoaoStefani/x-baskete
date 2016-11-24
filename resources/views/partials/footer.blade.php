d6d9d1fc69cbd2fa2ac2f512c0702170bf0e89a5
João Marcos Stefani <joaomstefani@gmail.com>
2016-11-24 18:03:00 -0200

    Alterações no CSS, e na home

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,14 +1,48 @@
-<div id="footer">
+<footer id="footer" style="background-color:#283e87">
     <div class="container">
-        <p class="text-muted credit">
-            <span style="text-align: left; float: left">&copy; <?php echo date('Y'); ?>
-                <a href="http://fcmoreno.com.br/" target="_blank">
-                    FC Moreno Tecnologia - Desenvolvimento de Sistemas
-                </a>
-            </span>
-            <span class="hidden-phone" style="text-align: right; float: right">Desenvolvido por:
-                Equipe PHP FC Moreno Tecnologia
-            </span>
-        </p>
+        {{--INICIO DO BLOCO DA DIREITA--}}
+        <div class="col-md-4">
+            <img src="/img/farmaisinho.png" class="img-responsive">
+            <p class="txt-footer">Av. Pres. Juscelino Kubitschek, 1830 | Torre 4 | 2º Andar</p>
+            <p class="txt-footer">CEP 04543 900 | Itaim Bibi | São Paulo - SP</p>
+            <div style="border-left:2px solid #adadad;height:241px;position:absolute;top:22px;right:-36px;"></div>
+        </div>
+        {{--FIM DO BLOCO DA DIREITA--}}
+
+        {{--INICIO DO BLOCO DO MEIO--}}
+        <div class="col-md-4" style="margin-top:22px;">
+            <p>
+                <a href="{{ url('') }}" class="menu-footer">INFORMAÇÕES</a>
+            </p>
+            <p>
+                <a href="{{ url('') }}" class="menu-footer">NEGÓCIOS</a>
+            </p>
+            <p>
+                <a href="{{ url('') }}" class="menu-footer">MATERIAIS DE LOJA</a>
+            </p>
+            <p>
+                <a href="{{ url('') }}" class="menu-footer">TRADE</a>
+            </p>
+            <p>
+                <a href="{{ url('') }}" class="menu-footer">CAMPO</a>
+            </p>
+            <p>
+                <a href="{{ url('') }}" class="menu-footer">MARKETING</a>
+            </p>
+            <p>
+                <a href="{{ url('') }}" class="menu-footer">EDUCAÇÃO CORPORATIVA</a>
+            </p>
+            <p>
+                <a href="{{ url('') }}" class="menu-footer">RECURSOS HUMANO</a>
+            </p>
+            <p>
+                <a href="{{ url('') }}" class="menu-footer">REGULATÓRIO</a>
+            </p>
+            <div style="border-left:2px solid #adadad;height:241px;position:absolute;top:0px;left:400px;"></div>
+        </div>
+        {{--FIM DO BLOCO DO MEIO--}}
+        {{--INICIO DO BLOCO DA ESQUERDA--}}
+       
+        {{--FIM DO BLOCO DA ESQUERDA--}}
     </div>
-</div>
+</footer>
