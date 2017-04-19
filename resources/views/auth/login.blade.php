c4cec73a4abf4c22f1e8a0d5b6d3087bfd8276f0
João Marcos Stefani <joaomstefani@gmail.com>
2017-04-19 15:37:53 -0300

    Alterações no CSS

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,53 +1,64 @@
-@extends('layouts.app')
-
-{{-- Web site Title --}}
+<!DOCTYPE html>
+<html>
 @section('title') Login :: @parent @endsection
-
-{{-- Content --}}
-@section('content')
-    <div class="row">
-        <div class="page-header">
-            <h2>Entre com sua conta</h2>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Amatic+SC">
+<link href="{{ asset('css/w3.css') }}" rel="stylesheet">
+<style>
+body, html {height: 100%}
+body,h1,h2,h3,h4,h5,h6 {font-family: "Amatic SC", sans-serif}
+.menu {display: none}
+.bgimg {
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-image: url("http://xbaskete.dev/fundo.jpg");
+    min-height: 100%;
+}
+</style>
+<body>
+<!-- Header with image -->
+<header class="bgimg w3-display-container w3-grayscale-min" id="home">
+    <div class="w3-display-middle w3-center">
+        <span class="w3-text-white w3-hide-small" style="font-size:100px">Entre com sua conta</span>
+        {!! Form::open(array('url' => url('auth/login'), 'method' => 'post', 'files'=> true)) !!}
+        <div class="form-group  {{ $errors->has('email') ? 'has-error' : '' }} col-md-6 col-md-offset-2">
+            {!! Form::label('email', "E-Mail Address", array('class' => 'w3-text-white w3-hide-small')) !!}
+            <div class="controls">
+                {!! Form::text('email', null, array('class' => 'w3-input w3-padding-16 w3-border')) !!}
+                <span class="help-block">{{ $errors->first('email', ':message') }}</span>
+            </div>
         </div>
-    </div>
-
-    <div class="container-fluid">
-        <div class="row">
-            {!! Form::open(array('url' => url('auth/login'), 'method' => 'post', 'files'=> true)) !!}
-            <div class="form-group  {{ $errors->has('email') ? 'has-error' : '' }} col-md-6 col-md-offset-2">
-                {!! Form::label('email', "E-Mail Address", array('class' => 'control-label')) !!}
-                <div class="controls">
-                    {!! Form::text('email', null, array('class' => 'form-control')) !!}
-                    <span class="help-block">{{ $errors->first('email', ':message') }}</span>
-                </div>
+        <div class="form-group  {{ $errors->has('password') ? 'has-error' : '' }} col-md-6 col-md-offset-2">
+            {!! Form::label('password', "Password", array('class' => 'w3-text-white w3-hide-small')) !!}
+            <div class="controls">
+                {!! Form::password('password', array('class' => 'form-control')) !!}
+                <span class="help-block">{{ $errors->first('password', ':message') }}</span>
             </div>
-            <div class="form-group  {{ $errors->has('password') ? 'has-error' : '' }} col-md-6 col-md-offset-2">
-                {!! Form::label('password', "Password", array('class' => 'control-label')) !!}
-                <div class="controls">
-                    {!! Form::password('password', array('class' => 'form-control')) !!}
-                    <span class="help-block">{{ $errors->first('password', ':message') }}</span>
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="col-md-6 col-md-offset-4">
-                    <div class="checkbox">
-                        <label>
-                            <input type="checkbox" name="remember"> Remember Me
-                        </label>
-                    </div>
-                </div>
-            </div>
-
-            <div class="form-group">
-                <div class="col-md-6 col-md-offset-4">
-                    <button type="submit" class="btn btn-primary" style="margin-right: 15px;">
-                        Login
-                    </button>
-
-                    <a href="{{ url('/password/email') }}">Esqueceu sua senha?</a>
-                </div>
-            </div>
-            {!! Form::close() !!}
         </div>
+        <div class="form-group">
+            <div class="col-md-6 col-md-offset-4">
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" name="remember"> Remember Me
+                    </label>
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <div class="col-md-6 col-md-offset-4">
+                <button type="submit" class="w3-button w3-xxlarge w3-black" style="margin-right: 15px;">
+                    Login
+                </button>
+
+                <a href="{{ url('/password/email') }}">Esqueceu sua senha?</a>
+            </div>
+        </div>
+        {!! Form::close() !!}
     </div>
-@endsection
+</header>
+</body>
+</html>
+
+
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>@section('title') X BASKETE @show</title>
+    <title>@section('title') X-BASKETE @show</title>
     @section('meta_keywords')
         <meta name="keywords" content="your, awesome, keywords, here"/>
     @show @section('meta_author')
@@ -15,12 +15,25 @@
               content="Lorem ipsum dolor sit amet, nihil fabulas et sea, nam posse menandri scripserit no, mei."/>
     @show
 
-		<link href="{{ asset('css/site.css') }}" rel="stylesheet">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Amatic+SC">
+
+		<link href="{{ asset('css/w3.css') }}" rel="stylesheet">
         <link rel="stylesheet" href="{{ asset('css/sweetalert.css')}}">
 
         <script src="{{ asset('js/site.js') }}"></script>
         <script src="{{ asset('js/jquery.mask.min.js') }}"></script>
         <script src="{{ asset('js/sweetalert.min.js')}}"></script>
+
+        <style>
+            body, html {height: 100%}
+            body,h1,h2,h3,h4,h5,h6 {font-family: "Amatic SC", sans-serif}
+            .bgimg {
+                background-repeat: no-repeat;
+                background-size: cover;
+                background-image: url(fundo.jpg);
+                min-height: 90%;
+            }
+        </style>
 
     @yield('styles')
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
